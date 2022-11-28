db2f27895bdf8044915c7ace04368bd66ae03a3e
andrea scacchi <andrea.scacchi@live.com>
2022-11-28 19:23:24 +0100

    Add project files.

---
+++ Models/Pizza.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        //properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+        public float Price { get; set; }
+
+        //empty constructor
+        public Pizza()
+        {
+
+        }
+
+
+        //constructor
+        public Pizza(string name, string description, string image, float price)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+    }
+}
