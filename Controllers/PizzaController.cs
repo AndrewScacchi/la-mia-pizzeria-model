2dfbac1520ba95b8ccb90e06c34b8d314c42c06d
andrea scacchi <andrea.scacchi@live.com>
2022-11-28 20:13:22 +0100

    Exercise Completed. Added Controller for Pizza, Pizza Model and relative Index and Detail view with specific ids

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,30 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            //query al db
+            PizzeriaDbContext db = new PizzeriaDbContext();
+            //grabbing all Pizzas in db 
+            //return View(db.Pizzas.ToList());
+
+            //store Pizza in an obj can be useful but require further inclusion
+            List<Pizza> listPizza = db.Pizzas.ToList();
+            return View(listPizza);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            PizzeriaDbContext db = new PizzeriaDbContext();
+
+            Pizza pizza = db.Pizzas.Where(p => p.Id == id).FirstOrDefault();
+
+            return View(pizza);
+        }
+    }
+}
